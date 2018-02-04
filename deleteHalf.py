5a16b04758c4158b888f7ee98102f088fd37b5a0
IsseiMori <ganntyu1225issei@gmail.com>
2018-02-04 00:20:54 -0800

    outline

---
+++ deleteHalf.py
@@ -0,0 +1,38 @@
+#deleteHalf.py 2017 Issei Mori
+#Python script for Autodesk Maya to delete the faces on the half side
+
+from maya import cmds
+
+#Delete the faces on the negative side of the axis
+def deleteHalf(node = None, axis = "x"):
+    #if no object is selected, return error
+
+    #for loop for every face
+        
+        #if the face has any negative vertex, delete
+
+class Window(object):
+    def __init__(self):
+        pass
+
+        #close the current window if the same name window exists
+
+        #open window
+
+        #build UI
+
+        #show window
+    
+    def buildUI():
+        pass
+
+        #set up column
+
+        #radio button for axis
+
+        #run button
+
+    def onApplyClick(self, *args):
+        #run deleteHalf method
+
+        #close window to end
